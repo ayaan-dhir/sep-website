4304fceae45c1baf351ce816d6c7ee78792aac66
pahel <pahels1@uci.edu>
2024-02-20 16:47:38 -0800

    added members page

---
+++ src/pages/members.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Members() {
+  return (
+    <div>
+      <h1>Members Page</h1>
+      <p>This is the members page of Sigma Eta Pi.</p>
+    </div>
+  );
+}
+
+export default Members;
