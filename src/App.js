6aac61c800f0905a018963b758cdc7f394f53508
pahel <pahels1@uci.edu>
2024-02-20 16:45:26 -0800

    added members page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './white-logo.png';
-// import black_logo from './black-logo.svg';
 import './App.css';
+import Members from './pages/members';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="header-logo" alt="logo" />
-        <div className="header-text">
-          <p>Startups</p> <p>Members</p> <p>Recruitment</p>
-        </div>
-      </header>
-    </div>
-
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="header-logo" alt="logo" />
+          <div className="header-text">
+            <p>Startups</p> 
+            <Link to="/members" className="header-link">Members</Link>
+            <p>Recruitment</p>
+          </div>
+        </header>
+        <Routes>
+            <Route path="/members" element={<Members />} />
+            {/* Define more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
