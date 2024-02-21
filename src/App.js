de8ce1efeac11a9e2eef88701b055e18c2bfb005
Ayaan Dhir <ayaand@uci.edu>
2024-02-20 16:38:25 -0800

    started header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './white-logo.png';
+// import black_logo from './black-logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='top'>Hello</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Sigma Eta Pi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="header-logo" alt="logo" />
+        <div className="header-text">
+          <p>Startups</p> <p>Members</p> <p>Recruitment</p>
+        </div>
       </header>
     </div>
+
   );
 }
 
