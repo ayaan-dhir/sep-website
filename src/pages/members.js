390a03dc008151157c8312dd9010b5c91e35763c
Ayaan Dhir <ayaand@uci.edu>
2024-02-23 17:15:27 -0800

    worked more on members page

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import './members.css'
-// import ayaan from './members/ayaan.jpg'
+import ayaan from './members/ayaan.jpg'
 import byren from './members/byren.jpg'
 import danielyi from './members/daniel.jpg'
 import michelle from './members/michelle.jpg'
 import shreya from './members/shreya.jpg'
+import rithwik from './members/rithwik.jpg'
 
 const membersData = [
     {
       id: 1,
+      position: 'President',
       name: 'Shreya Mawandia',
       photo: shreya
     },
     {
       id: 2,
+      position: 'VP of External Affairs',
       name: 'Byren Cheema',
       photo: byren
     },
 
     {
       id: 3,
+      position: 'VP of Internal Affairs',
       name: 'Daniel Yi',
       photo: danielyi
     },
     
     {
       id: 4,
+      position: 'VP of Marketing',
       name: 'Michelle Lei',
       photo: michelle
     },
+
+    {
+      id: 5,
+      position: 'VP of Finance',
+      name: 'Rithwik Kerur',
+      photo: rithwik
+    },
+
+    {
+      id: 6,
+      position: 'VP of Founder\'s Education',
+      name: 'Ayaan Dhir',
+      photo: ayaan
+    },
+
   ];
 
 function Members() {
@@ -39,6 +59,7 @@ function Members() {
         {membersData.map((member) => (
           <div key={member.id} className="member-item">
             <img src={member.photo} alt={member.name} className="member-photo" />
+            <div className="member-position">{member.position}</div>
             <div className="member-name">{member.name}</div>
           </div>
         ))}
