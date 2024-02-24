edee07bab6aaf6823d4683fec93be2ca45cef7ca
Ayaan Dhir <ayaand@uci.edu>
2024-02-23 19:11:01 -0800

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './white-logo.png';
 import './App.css';
@@ -9,6 +9,12 @@ import FoundersEducation from './pages/founders_education';
 import Typewriter from "typewriter-effect";
 
 function App() {
+  // State to manage dropdown visibility
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  // Function to toggle dropdown visibility
+  const toggleDropdown = () => setIsDropdownVisible(!isDropdownVisible);
+
   return (
     <Router>
       <div className="App">
@@ -16,7 +22,18 @@ function App() {
           <Link to="/"><img src={logo} className="header-logo" alt="logo" /></Link>
           <div className="header-text">
             <Link to="/startups" className="header-link">Startups</Link>
-            <Link to="/members" className="header-link">Members</Link>
+
+            <div className="header-link dropdown">
+              <div onClick={toggleDropdown}>Members</div>
+              {/* Dropdown Menu */}
+              {isDropdownVisible && (
+                <div className="dropdown-content">
+                  <Link to="/members/board" className="dropdown-item">Board</Link>
+                  <Link to="/members/startups" className="dropdown-item">Startups</Link>
+                  <Link to="/members/founders_education" className="dropdown-item">Founder's Education</Link>
+                </div>
+              )}
+            </div>
             <Link to="/founders_education" className="header-link">Founder's Education</Link>
           </div>
         </header>
@@ -41,8 +58,10 @@ function App() {
             </div>
           } />
           <Route path="/startups" element={<Startups />} />
-          <Route path="/members" element={<Members />} />
-          <Route path="/founders_education" element={<FoundersEducation />} />
+          {/* Update the paths as necessary */}
+          <Route path="/members/board" element={<Members />} />
+          <Route path="/members/startups" element={<Startups />} />
+          <Route path="/members/founders_education" element={<FoundersEducation />} />
         </Routes>
       </div>
     </Router>
