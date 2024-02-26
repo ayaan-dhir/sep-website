9a1e61933ec2c2418d130cdc2b7012841593dd6a
Ayaan Dhir <ayaand@uci.edu>
2024-02-26 15:39:02 -0800

    note from FE section

--- src/pages/fe.js
+++ src/pages/fe.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './fe.css'
 
+import ayaan from './members/alpha/ayaan.jpg'
+
 const curriculumItems = [
   { title: 'Problem Discovery' },
   { title: 'Customer Discovery' },
@@ -20,8 +22,8 @@ function FoundersEducation() {
         <p className='fe-subtext'>Our Startup Accelerator</p>
       </div>
       <div className='fe-pages'>
-      <h1 className='headings'>The Program</h1>
-      <p className='fe-subtext'>Founder's Education is our 8-week startup incubator program focused on helping you think and build like an entrepreneur.</p>
+        <h1 className='headings'>The Program</h1>
+        <p className='fe-subtext'>Founder's Education is our 8-week startup incubator program focused on helping you think and build like an entrepreneur.</p>
       </div>
       <div className='fe-pages'>
         <h1 className='headings'>Our Curriculum</h1>
@@ -35,6 +37,20 @@ function FoundersEducation() {
           </div>
         </div>
       </div>
+      <div className='fe-note-page'>
+        <h1 className='headings'>A Note from Our FE</h1>
+        <div className="fe-content-wrapper"> {/* Wrapper for image and text */}
+          <div className="fe-photo"><img src={ayaan} alt="Founder's Ed"></img></div>
+          <div className="fe-text">
+            <p>"Hi everyone! My name is Ayaan and I focus on building startups through our Founder's Education, our program designed to help you chase after your entrepreneurial endeavors.
+              Beyond our curriculum, our program is heavily focused on collaboration and community as you get to build alongside your cohort. What makes FE so special is that it caters to all experience levels. 
+              We have had members come in with no experience who later go on to pursue entrepreneurship full-time and we also have members who have started ventures before joining who find their co-founders for their next startup.
+              I highly encourage people to come to Sigma Eta Pi if you are curious about entrepreneurship and want to have a lasting impact on your community. FE is the place where innovators at UCI come to build!"
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
